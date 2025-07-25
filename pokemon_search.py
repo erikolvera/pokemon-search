cba45ce607483ef083ddd368b09dea755683642e
Erik Olvera <erikolvera44@gmail.com>
2025-07-25 10:57:30 -0500

    finished adding implementation to search up by name/species

--- pokemon_search.py
+++ pokemon_search.py
@@ -7,45 +7,45 @@ base_url ="https://pokeapi.co/api/v2"
 def search_pokemon_name(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-
     if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
+        return response.json()
+    return None
         
+#test if searching up by pokemon species worked
 def search_pokemon_species(name):
     url = f"{base_url}/pokemon-species/{name}"
     response = requests.get(url)
-
     if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
+        return response.json()
+    return None
 
-def get_pokemon_data(name):
+#call both api requests at the same time
+def get_pokemon_data(user_input):
     with ThreadPoolExecutor() as executor:
-        info_future = executor.submit(search_pokemon_name, name)
-        species_future = executor.submit(search_pokemon_species, name)
-        info = info_future.result()
+        name_future = executor.submit(search_pokemon_name, user_input)
+        species_future = executor.submit(search_pokemon_species, user_input)
+        name = name_future.result()
         species = species_future.result()
-    if info:
-        return info, None
+    #runs if searching up by the pokemon name was successful
+    if name:
+        return name, None
+    ##runs if searching up by the pokemon name fails, but the species was found
     elif species:
         return None, species
+    #if both name and species fail
     else:
         return None, None
 
-pokemon_name = input("Search up Pokemon name: ")
+#user is asked to enter Pokemon name
+pokemon_name = input("Search up Pokemon: ")
 pokemon_info, pokemon_species = get_pokemon_data(pokemon_name)
 
 if pokemon_info:
-    print(f"Name: {pokemon_info["name"].capitalize()}")
-    print(f"Pokedex #: {pokemon_info["id"]}")
+    print(f"Name: {pokemon_info['name'].capitalize()}")
+    print(f"Pokedex #: {pokemon_info['id']}")
     # print(f"{pokemon_info["height"]}")
 elif pokemon_species:
-    print(f"Name: {pokemon_species["name"].capitalize()}")
-    print(f"Pokedex #: {pokemon_species["id"]}")
+    print(f"Name: {pokemon_species['name'].capitalize()}")
+    print(f"Pokedex #: {pokemon_species['id']}")
 else:
     print("Pokemon not found.")
