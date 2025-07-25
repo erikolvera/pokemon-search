1502af781d5f73792d1f95b8127669ef4bd14f39
Erik Olvera <erikolvera44@gmail.com>
2025-07-25 10:14:50 -0500

    successfully conntected pokeapi and completed pokemon name and information retrieval

---
+++ pokemon_search.py
@@ -0,0 +1,31 @@
+import requests
+
+base_url ="https://pokeapi.co/api/v2"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+        
+# def get_pokemon_species(name):
+#     url = f"{base_url}/pokemon-species/{name}"
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#         pokemon_data = response.json()
+#         return pokemon_data
+#     else:
+#         print(f"Failed to retrieve data {response.status_code}")
+
+pokemon_name = input("Search up Pokemon name: ")
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"].capitalize()}")
+    print(f"Pokedex #: {pokemon_info["id"]}")
+    # print(f"{pokemon_info["height"]}")
